1d06cf24f8d3ec6337b03d57e4cd780ce10af00e
DUKT Squad - SHYNZ Development Team <duket@shynz.com>
2023-12-17 13:32:51 -0500

    Version 1.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, embeddedWallet, metamaskWallet, coinbaseWallet, smartWallet } from "@thirdweb-dev/react";
 import { Navbar } from "../components/Navbar/Navbar";
 import NextNProgress from "nextjs-progressbar";
 import { NETWORK } from "../const/contractAddresses";
 import "../styles/globals.css";
+import { ACCOUNT_FACTORY_ADDRESS } from "../const/contractAddresses";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const smartWalletConfig = {
+    factoryAddress: ACCOUNT_FACTORY_ADDRESS,
+    gasless: true,
+  }
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
